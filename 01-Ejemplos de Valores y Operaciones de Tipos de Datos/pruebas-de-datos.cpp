cec57f5c7cf5d0e61979fa4380953a054104c36b
Macarena Ugolini <103282154+m-ugolini@users.noreply.github.com>
2022-05-09 23:01:20 -0300

    Add files via upload

---
+++ 01-Ejemplos de Valores y Operaciones de Tipos de Datos/pruebas-de-datos.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std::literals;
+
+int main ()
+{
+    // prueba bool
+    assert(false==false);
+    assert(true);
+    assert(not false);
+    assert(not false == true);
+    assert (false or true);
+    assert (true != false);
+    assert (true or true and false);
+    // prueba double 
+    assert (2.0 == 1.0 + 1.0);
+    assert (1 == 1.0);
+    assert (2 == 1.0 + 1);
+    // prueba string
+    assert ("algoritmos"s == "algo"s + "ritmos"s);
+    assert ("algoritmos"s.length() == 10);
+    // prueba int
+    assert (4 == 3 + 1);
+    assert (4 == 8 / 2);
+    assert (10 == 6 + 4);
+    // prueba char
+    assert ( 97 == 'a');
+    assert (':' == 58);
+    assert ('Y' == 89 );
+}
